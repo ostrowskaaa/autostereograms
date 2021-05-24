70cd590045a3ca8fb69b0e4051ffa1944895063e
ostrowskaaa <50330163+ostrowskaaa@users.noreply.github.com>
2021-05-25 01:08:04 +0200

    Update autostereograms.py

--- autostereograms.py
+++ autostereograms.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import streamlit as st
 from PIL import Image
+from scipy.signal import argrelextrema
 
 
 def image_difference(vec, image):
@@ -12,20 +13,39 @@ def image_difference(vec, image):
     return difference
 
 
+def local_extremums(black_pix_array):
+    minimums = argrelextrema(black_pix_array, np.less)
+    maximums = argrelextrema(black_pix_array, np.greater)
+    return minimums[0], maximums[0]
+
+
+def biggest_extremums_diff(minimums, maximums, black_pix_list):
+    value, vec = 0, 0
+    for i in range(min(len(minimums), len(maximums))):
+        minn, maxx = black_pix_list[minimums[i]], black_pix_list[maximums[i]]
+        if (maxx - minn) > value:
+            value, vec = maxx - minn, maximums[i]
+    return vec
+
+
+def count_black_pix(image, cropped_img, vec):
+    grayImage = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
+    black_pix = (image.shape[0] * (image.shape[1] - vec)) - cv2.countNonZero(grayImage)
+    return black_pix
+
+
 def solve_stereogram(image):
     image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    black_pix = 0
-    answer_img = None
+    black_pix_list = []
     vec = int(image.shape[1] * 0.1)
     while vec < image.shape[1] * 0.4 :
         difference = image_difference(vec, image)
-        crop_img = difference[0:image.shape[0], vec:image.shape[1]]
-        grayImage = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-        count_black_pix = (image.shape[0]*(image.shape[1]-vec)) - cv2.countNonZero(grayImage)
-        if count_black_pix > black_pix:
-            black_pix = count_black_pix
-            answer_img = difference
+        crop_img = difference[0 : image.shape[0], vec : image.shape[1]]
+        black_pix_list.append(count_black_pix(image, crop_img, vec))
         vec += 1
+    minimums, maximums = local_extremums(np.array(black_pix_list))
+    shift_value = biggest_extremums_diff(minimums, maximums, black_pix_list)
+    answer_img = image_difference(int(image.shape[1] * 0.1) + shift_value, image)
     return answer_img
 
 
@@ -43,8 +63,8 @@ st.write("""
     it's not a perfect algorithm so not every autostereogram will be solved but I'm working on improving it!
 """)
 
-uploaded_file = st.file_uploader('Upload Files',type=['png','jpeg', 'jpg'])
 
+uploaded_file = st.file_uploader('Upload Files',type=['png','jpeg', 'jpg'])
 if uploaded_file is not None:
     file_details = {'FileName':uploaded_file.name,
                     'FileType':uploaded_file.type,
